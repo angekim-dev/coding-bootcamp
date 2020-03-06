aff3277f2b697b341bb1a18647cd1a04eafa1ec4
ANGELIKA KIM <alika852@gmail.com>
2020-03-06 13:47:54 +0100

    uploading in-class-exercise

---
+++ in-class-exercise.js
@@ -0,0 +1,22 @@
+var x = 0;
+var doublex = 0;
+
+function timesTwo(num) {
+    return num * 2;
+}
+
+var x = 15;
+var doublex = timesTwo(x);
+
+var numbers = 0;
+
+numbers = [x, doublex];
+
+for (var i = 0; i > numbers.length; i++) {
+    console.log(numbers);
+    return numbers = {};
+}
+
+numbers = {
+    y: doublex;
+}
