06ca3a58cf813c4536139f3821a82c61af63cc5e
ANGELIKA KIM <alika852@gmail.com>
2020-03-09 00:16:59 +0100

    finished exercises topic: constuctors, prototypes & this-keyword

--- exercise4.1.js
+++ exercise4.1.js
@@ -3,16 +3,26 @@ function Rectangle(w, h) {
     this.height = h;
 }
 
+Rectangle.prototype.getArea = function getArea() {
+    return this.width * this.height;
+};
+
 function Square(n) {
     this.width = n;
     this.height = n;
 }
 
-return this.width * this.height;
+Square.prototype = new Rectangle();
 
-//Write a constructor called Rectangle
+var rect = new Rectangle(4, 5);
+console.log(rect.getArea()); //20
+var square = new Square(4);
+console.log(square.getArea()); //16
+
+//Write a constructor (function of prototype) called Rectangle
 //that accepts two numbers (width and height) as parameters.
-//Rectangle instances should have a method called getArea
+//Rectangle instances (objects created with new)
+//should have a method (function belonging to object) called getArea
 //that returns the instance's width multiplied by its height.
 //Write another constructor called Square that accepts one number
 //(which will serve as both width and the height) as a parameter.
@@ -20,9 +30,3 @@ return this.width * this.height;
 //but you should not rewrite the getArea function you wrote for Rectangle.
 //Square instances should use the same getArea method
 //that Rectangle instances do.
-
-// var square = new Square(4);
-// square.getArea(); //16
-
-// var rect = new Rectangle(4, 5);
-// rect.getArea(); //20
--- exercise4.2.js
+++ exercise4.2.js
@@ -1,1 +1,24 @@
-// function invertCase(str) {}
+function invertCase(str) {
+    var newStr = "";
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == str[i].toUpperCase()) {
+            newStr += str[i].toLowerCase();
+        } else {
+            newStr += str[i].toUpperCase();
+        }
+    }
+    return newStr;
+}
+
+console.log(invertCase("hEllo upSiDeDOWNworld"));
+
+//Write a function called invertCase
+//that expects a string as a parameter.
+//This function should return a new string
+//with all the same characters as the string that was passed in
+//but with the cases of the alphabetic characters switched.
+//Uppercase characters should become lowercase
+//and lowercase letters should become uppercase.
+//Characters that are not alphabetic should not change.
+//The toUpperCase and toLowerCase methods that all strings have
+//will come in handy here.
