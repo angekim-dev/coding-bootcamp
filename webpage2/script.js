65f3e958fa76cfd08909479f04b31498f733472e
ANGELIKA KIM <alika852@gmail.com>
2020-03-11 20:25:31 +0100

    Welcome To The Future Website - hamburger menu works, no animation

---
+++ webpage2/script.js
@@ -0,0 +1,23 @@
+var hamburger = document.getElementById("menu");
+var hm = document.getElementById("hamburger-menu");
+var closingX = document.getElementById("x");
+var body = document.querySelector("body");
+var sideMenu = document.getElementById("menu-side");
+
+hamburger.addEventListener("click", function() {
+    event.stopPropagation();
+    hm.classList.add("on");
+});
+
+closingX.addEventListener("click", function() {
+    event.stopPropagation();
+    hm.classList.remove("on");
+});
+
+body.addEventListener("click", function() {
+    hm.classList.remove("on");
+});
+
+sideMenu.addEventListener("click", function() {
+    event.stopPropagation();
+});
