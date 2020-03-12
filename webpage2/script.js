b41c3ca9b8c656b686721a3f03d70667e1b76544
ANGELIKA KIM <alika852@gmail.com>
2020-03-12 23:48:04 +0100

    added slide feature to webside2 (works only for 1st click)

--- webpage2/script.js
+++ webpage2/script.js
@@ -1,23 +1,26 @@
-var hamburger = document.getElementById("menu");
-var hm = document.getElementById("hamburger-menu");
-var closingX = document.getElementById("x");
-var body = document.querySelector("body");
-var sideMenu = document.getElementById("menu-side");
+(function() {
+    var hamburger = document.getElementById("menu");
+    var hm = document.getElementById("hamburger-menu");
+    var closingX = document.getElementById("x");
+    var body = document.querySelector("body");
+    var sideMenu = document.getElementById("menu-side");
 
-hamburger.addEventListener("click", function() {
-    event.stopPropagation();
-    hm.classList.add("on");
-});
+    hamburger.addEventListener("click", function() {
+        event.stopPropagation();
+        hm.classList.add("on");
+        sideMenu.classList.add("onscreen");
+    });
 
-closingX.addEventListener("click", function() {
-    event.stopPropagation();
-    hm.classList.remove("on");
-});
+    closingX.addEventListener("click", function() {
+        event.stopPropagation();
+        hm.classList.remove("on");
+    });
 
-body.addEventListener("click", function() {
-    hm.classList.remove("on");
-});
+    body.addEventListener("click", function() {
+        hm.classList.remove("on");
+    });
 
-sideMenu.addEventListener("click", function() {
-    event.stopPropagation();
-});
+    sideMenu.addEventListener("click", function() {
+        event.stopPropagation();
+    });
+})();
