26d08dc205d99c60b1e04405ed88745b1c63554a
ANGELIKA KIM <alika852@gmail.com>
2020-03-13 21:02:36 +0100

    kitty-carousel finished

--- canvas/script.js
+++ canvas/script.js
@@ -1,34 +1,50 @@
 (function() {
     var c = document.querySelector("canvas");
     var ctx = c.getContext("2d");
+    // var position = {
+    //     x: 200,
+    //     y: 100
+    // };
+
+    // var velocity = 1;
+
+    // draw();
+
+    // function draw() {
+    // ctx.clearRect(0, 0, 400, 400);
+    // position.x += velocity;
+    // position.y += velocity;
+
+    // if (position.x >= 215 || position.y >= 350) {
+    //     velocity = -1;
+    // } else if (position.x < 50 || position.y < 80) {
+    //     velocity = 1;
+    // }
     ctx.beginPath();
-    ctx.moveTo(100, 100);
+    ctx.moveTo(100, 230);
     ctx.lineTo(200, 200);
-    ctx.closePath();
     ctx.strokeStyle = "tomato";
+    ctx.lineWidth = 5;
     ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(200, 100, 50, 0, (360 * Math.PI) / 180);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.beginPath();
     ctx.moveTo(100, 350);
     ctx.lineTo(200, 250);
-    ctx.closePath();
     ctx.stroke();
-    ctx.beginPath();
     ctx.moveTo(300, 100);
     ctx.lineTo(200, 200);
-    ctx.closePath();
     ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(200, 250);
-    ctx.lineTo(200, 200);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.beginPath();
     ctx.moveTo(300, 350);
     ctx.lineTo(200, 250);
-    ctx.closePath();
     ctx.stroke();
+    ctx.moveTo(200, 250);
+    ctx.lineTo(200, 200);
+    ctx.stroke();
+
+    ctx.beginPath();
+
+    ctx.arc(200, 100, 50, 0, (360 * Math.PI) / 180);
+
+    ctx.stroke();
+
+    // requestAnimationFrame(draw);
+    // }
 })();
--- kitty-carousel/script.js
+++ kitty-carousel/script.js
@@ -11,27 +11,24 @@
 
     function clickHandler(dotIndex) {
         return function() {
-            if (isTransitioning === true) {
-                //if the transition is happening -> do nothing
+            if (isTransitioning || dotIndex == currentCat) {
                 return;
             }
             clearTimeout(timer);
             moveKitties(dotIndex);
-        }; /*what should happen when event happens*/
+        };
     }
 
     function moveKitties(index) {
         console.log("index: ", index);
 
         kitties[currentCat].classList.remove("onscreen");
+        dots[currentCat].classList.remove("on");
         kitties[currentCat].classList.add("offscreen-left");
 
         if (typeof index === "number") {
-            //if-else block location matters!!
-            //user clicked a dot, something must happen
             currentCat = index;
         } else {
-            // user didn't touch it, let the carousel run on its own
             currentCat++;
         }
 
@@ -39,21 +36,17 @@
             currentCat -= kitties.length;
         }
 
-        kitties[currentCat].classList.add("onscreen"); //task: find out how to make it work without writing several functions
-        // currentCat++;
-        // console.log(currentCat);
-        // timer = setTimeout(moveKitties, 3000); // might leave screen empty first, but transitionend will fix it
+        kitties[currentCat].classList.add("onscreen");
+        dots[currentCat].classList.add("on");
+        isTransitioning = true;
     }
 
     document.addEventListener("transitionend", function(e) {
         if (e.target.className == "offscreen-left") {
             e.target.classList.remove("offscreen-left");
+            isTransitioning = false;
             timer = setTimeout(moveKitties, 3000);
         }
-        // console.log(e.target);
-        // will be triggered whenever a transition finishes
-        // including transition from onscreen to offscreen left
-        // makes the carousel run forever
     });
 
     timer = setTimeout(moveKitties, 3000);
