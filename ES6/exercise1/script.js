2b52d5c8d14321d9f00edf7cd72ce46480ddf1d1
ANGELIKA KIM <alika852@gmail.com>
2020-03-25 13:41:42 +0100

    first es6 exercise done

---
+++ ES6/exercise1/script.js
@@ -0,0 +1,16 @@
+function fn(arr) {
+    const newArr = arr.reverse();
+    return newArr;
+}
+
+console.log(fn([10, 20, 30]));
+
+// Write a function that takes an array as an argument
+//and returns a new array containing all of the items
+//that are in the array that was passed in
+//but in reverse order.
+//This function should
+// leave the original array unchanged
+// contain no loops of any kind. That includes for, for...in, for...of, while, and do...while
+// not call slice or any other method on the original array
+// not call push or concat on any array in any way
