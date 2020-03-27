be549c049fe84d6906e73c01662731cedb1f289c
ANGELIKA KIM <alika852@gmail.com>
2020-03-27 18:18:22 +0100

    output for part1 of exercise works

--- Week4/fun-with-fs/index.js
+++ Week4/fun-with-fs/index.js
@@ -3,37 +3,51 @@ const fs = require("fs");
 // You will be using its readdir and stat methods in Part 1
 
 function logSizes(path) {
-    const contents = fs.readdir(
-        __dirname,
+    const content = fs.readdir(
+        path,
         {
             withFileTypes: true
         },
         (err, items) => {
             if (err) {
-                console.log(err, "ERROR!!!");
+                console.log(err, "readdir ERROR!!!");
             } else {
-                console.log(items);
-                // for (let i in items) {
-                //     console.log(i);
-
-                fs.stat(__filename, (err, stats) => {
-                    if (err) {
-                        console.log("ERROR");
-                    } else {
-                        // console.log(stats.size);
-                        console.log(path, ": ", stats.size);
+                // console.log("items inside __dirname: ", items);
+                //
+                // console.log(item.name, item.isFile(), item.isDirectory());
+                for (const item of items) {
+                    const pathAndName = path + "/" + item.name;
+                    if (item.isFile() == true) {
+                        fs.stat(pathAndName, (err, file) => {
+                            if (err) {
+                                console.log("isFile ERROR", err);
+                            } else {
+                                console.log(pathAndName + ":" + file.size);
+                            }
+                        });
+                    } else if (item.isDirectory() == true) {
+                        logSizes(pathAndName);
                     }
-                });
+                }
+                // if (item.isFile() == true) {
+                //     fs.stat(__filename, (err, stats) => {
+                //         if (err) {
+                //             console.log("ERROR");
+                //         } else {
+                //             // console.log(stats.size);
+                //             console.log(path, "filename: ", stats.size);
+                //         }
+                //     });
+                // } else {
+                //     console.log("not a file");
+                // }
                 // }
             }
         }
     );
 }
 
-console.log(
-    logSizes("/Users/angelikakim/spsiced/msg-code/Week4/fun-with-fs/files")
-);
-// logSizes(__dirname + "/files");
+logSizes(__dirname + "/files");
 
 // its readdirSync, statSync and writeFileSync methods in Part 2.
 // function mapSizes(/Users/angelikakim/spiced/msg-code/Week4/fun-with-fs) {
