2a320883363b8166e3717165b03f1a1d57f32881
ANGELIKA KIM <alika852@gmail.com>
2020-03-29 17:47:47 +0200

    second readdir exersice: return sizes and empty object, so far

--- Week4/fun-with-fs/index.js
+++ Week4/fun-with-fs/index.js
@@ -2,62 +2,65 @@ const fs = require("fs");
 
 // You will be using its readdir and stat methods in Part 1
 
-function logSizes(path) {
-    const content = fs.readdir(
+// function logSizes(path) {
+//     fs.readdir(
+//         path,
+//         {
+//             withFileTypes: true
+//         },
+//         (err, items) => {
+//             if (err) {
+//                 console.log(err, "readdir ERROR!!!");
+//             } else {
+//                 // console.log("items inside __dirname: ", items);
+//                 //
+//                 // console.log(item.name, item.isFile(), item.isDirectory());
+//                 for (const item of items) {
+//                     const pathAndName = path + "/" + item.name;
+//                     if (item.isFile() == true) {
+//                         fs.stat(pathAndName, (err, file) => {
+//                             if (err) {
+//                                 console.log("isFile ERROR", err);
+//                             } else {
+//                                 console.log(pathAndName + ":" + file.size);
+//                             }
+//                         });
+//                     } else if (item.isDirectory() == true) {
+//                         logSizes(pathAndName);
+//                     }
+//                 }
+//             }
+//         }
+//     );
+// }
+
+// logSizes(__dirname + "/files");
+
+// its readdirSync, statSync and writeFileSync methods in Part 2.
+function mapSizes(path) {
+    const obj = {};
+    const items = fs.readdirSync(
+        //flow is blocked until the whole file is read
         path,
         {
             withFileTypes: true
-        },
-        (err, items) => {
-            if (err) {
-                console.log(err, "readdir ERROR!!!");
-            } else {
-                // console.log("items inside __dirname: ", items);
-                //
-                // console.log(item.name, item.isFile(), item.isDirectory());
-                for (const item of items) {
-                    const pathAndName = path + "/" + item.name;
-                    if (item.isFile() == true) {
-                        fs.stat(pathAndName, (err, file) => {
-                            if (err) {
-                                console.log("isFile ERROR", err);
-                            } else {
-                                console.log(pathAndName + ":" + file.size);
-                            }
-                        });
-                    } else if (item.isDirectory() == true) {
-                        logSizes(pathAndName);
-                    }
-                }
-                // if (item.isFile() == true) {
-                //     fs.stat(__filename, (err, stats) => {
-                //         if (err) {
-                //             console.log("ERROR");
-                //         } else {
-                //             // console.log(stats.size);
-                //             console.log(path, "filename: ", stats.size);
-                //         }
-                //     });
-                // } else {
-                //     console.log("not a file");
-                // }
-                // }
-            }
         }
     );
+    for (const item of items) {
+        const pathAndName = path + "/" + item.name;
+        if (item.isFile() == true) {
+            const stat = fs.statSync(pathAndName);
+            console.log(stat.size);
+        } else if (item.isDirectory() == true) {
+            mapSizes(pathAndName);
+        }
+    }
+    //do it al
+    // console.log(items.name, item.isFile(), item.isDirectory());
+    return obj;
 }
-
-logSizes(__dirname + "/files");
-
-// its readdirSync, statSync and writeFileSync methods in Part 2.
-// function mapSizes(/Users/angelikakim/spiced/msg-code/Week4/fun-with-fs) {
-//     const obj = {};
-
-//     //do it al
-
-//     return obj;
-// }
-// // once mapSizes works correctly
+console.log(mapSizes(__dirname + "/files"));
+// once mapSizes works correctly
 // fs.writeFile(
 //     "files.json",
 
@@ -66,4 +69,4 @@ logSizes(__dirname + "/files");
 //     4
 // );
 
-//JSON.stringify(obj, null, 4)
+// JSON.stringify(obj, null, 4);
