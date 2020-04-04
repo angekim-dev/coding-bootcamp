fae55457ce247d487683d50ccfb3cfb747728928
ANGELIKA KIM <alika852@gmail.com>
2020-04-05 01:31:46 +0200

    first rough version finished

--- Week5/ehportfolio/index.js
+++ Week5/ehportfolio/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const hb = require("express-handlebars");
 const projects = require("./projects.json");
-console.log(projects);
+// console.log(projects);
 //You will need to require this json file in your index.js, in order to then pass it to the template for use.
 
 //setting handlebars as the view engine
@@ -15,8 +15,21 @@ app.use(express.static("./public"));
 app.get("/", (req, res) => {
     res.render("welcome", {
         layout: "main",
-        projects
+        projects,
     });
+});
+
+app.get("/:project/description", (req, res) => {
+    const project = req.params.project;
+    const selectedProject = projects.find((item) => item.directory == project);
+    if (!selectedProject) {
+        return res.sendStatus(404);
+    } else {
+        res.render("description", {
+            projects,
+            selectedProject,
+        });
+    }
 });
 
 app.listen(8080, () => console.log("server listening"));
