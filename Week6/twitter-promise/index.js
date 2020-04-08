cbd8ceee22c793b6d91f71199be9ac7fd5db51ed
ANGELIKA KIM <alika852@gmail.com>
2020-04-08 10:52:54 +0200

    promise-ticker-tiwitter kind of works

--- Week6/twitter-promise/index.js
+++ Week6/twitter-promise/index.js
@@ -8,36 +8,42 @@ getTweets = promisify(getTweets);
 app.use(express.static("./ticker3"));
 
 app.get("/ticker3.json", (req, res) => {
-    getToken()
-        .then((bearerToken) => {
-            return Promise.all([
-                getTweets(bearerToken, "Drake"),
-                getTweets(bearerToken, "Beyonce"),
-                getTweets(bearerToken, "taylorswift13"),
-            ])
-                .then((result) => {
-                    // const drake = result[0];
-                    // const beyonce = result[1];
-                    // const taylor = result[2];
-                    const [drake, beyonce, taylor] = result;
-                    const mergedResults = drake.concat(beyonce, taylor);
-                    // OR const mergedResults == [...drake, ...beyonce, ...taylor];
-                    console.log("mergedResults:", mergedResults);
-                    const sortedTweets = mergedResults.sort((a, b) => {
-                        return new Date(b.created_at) - new Date(a.created_at);
-                    });
-                    const filteredTweets = filterTweets(sortedTweets);
-                    res.json(filteredTweets);
-                })
-                .catch((err) => console.log("err:", err));
-        })
-        .then((results) => {
-            console.log(results);
-        })
-        .catch((err) => {
-            console.log("err in catch", err);
-            res.sendStatus(500);
-        });
+    console.log("JSON");
+    getToken().then((bearerToken) => {
+        console.log(bearerToken);
+        return Promise.all([
+            getTweets(bearerToken, "Missy_Magazine"),
+            getTweets(bearerToken, "derfreitag"),
+            getTweets(bearerToken, "katie_willcox"),
+        ])
+            .then((result) => {
+                // const drake = result[0];
+                // const beyonce = result[1];
+                // const taylor = result[2];
+                const mergedResults = [
+                    ...result[0],
+                    ...result[1],
+                    ...result[2],
+                ];
+                // OR const mergedResults == [...drake, ...beyonce, ...taylor];
+                console.log("mergedResults:", mergedResults);
+                const sortedTweets = mergedResults.sort((a, b) => {
+                    return new Date(b.created_at) - new Date(a.created_at);
+                });
+                return sortedTweets;
+            })
+            .then((sortedTweets) => {
+                return filterTweets(sortedTweets);
+            })
+            .then((filteredTweets) => {
+                res.json(filteredTweets);
+            })
+            .catch((err) => console.log("err:", err));
+    });
+    // .catch((err) => {
+    //     console.log("err in getToken", err);
+    //     res.sendStatus(500);
+    // });
 });
 
 app.listen(8080, () => console.log("twitter ticker up"));
--- Week6/twitter-promise/twitter.js
+++ Week6/twitter-promise/twitter.js
@@ -18,20 +18,22 @@ module.exports.getToken = (callback) => {
     };
 
     const cbToken = function (responseToken) {
-        if (responseToken.statusCode != 200) {
-            // console.log("responseToken.statusCode ", responseToken.statusCode);
-            callback(responseToken.statusCode);
-            return;
-        }
-        let body = "";
-        responseToken.on("data", (chunk) => {
-            body += chunk;
-        });
+        return new Promise((resolve, reject) => {
+            if (responseToken.statusCode != 200) {
+                // console.log("responseToken.statusCode ", responseToken.statusCode);
+                callback(responseToken.statusCode);
+                return;
+            }
+            let body = "";
+            responseToken.on("data", (chunk) => {
+                body += chunk;
+            });
 
-        responseToken.on("end", () => {
-            let parsedBody = JSON.parse(body);
-            // console.log("parsedBody ...", parsedBody);
-            callback(null, parsedBody.access_token);
+            responseToken.on("end", () => {
+                let parsedBody = JSON.parse(body);
+                // console.log("parsedBody ...", parsedBody);
+                callback(null, parsedBody.access_token);
+            });
         });
     };
 
@@ -40,7 +42,7 @@ module.exports.getToken = (callback) => {
     reqToken.end("grant_type=client_credentials");
 };
 
-module.exports.getTweets = (bearerToken, callback) => {
+module.exports.getTweets = (bearerToken, maybeHere, callback) => {
     //similar to above
     // this function gets the tweets from twitter api
 
@@ -48,29 +50,26 @@ module.exports.getTweets = (bearerToken, callback) => {
         method: "GET",
         // screen_name: "Missy_Magazine",
         host: "api.twitter.com",
-        path:
-            "/1.1/statuses/user_timeline.json?screen_name=Missy_Magazine&tweet_mode=extended",
+        path: `/1.1/statuses/user_timeline.json?screen_name=${maybeHere}&tweet_mode=extended`,
         headers: {
             Authorization: "Bearer " + bearerToken,
         },
     };
 
     const cbTweets = function (resTweets) {
-        return new Promise((resolve, reject) => {
-            if (resTweets.statusCode != 200) {
-                // console.log("resTweets.statusCode: ", resTweets.statusCode);
-                resolve(resTweets.statusCode);
-                return;
-            }
-            let bodyTweets = "";
-            resTweets.on("data", (chunk) => {
-                bodyTweets += chunk;
-            });
-            resTweets.on("end", () => {
-                let tweets = JSON.parse(bodyTweets);
-                // console.log("tweets ...", tweets);
-                resolve(null, tweets);
-            });
+        if (resTweets.statusCode != 200) {
+            // console.log("resTweets.statusCode: ", resTweets.statusCode);
+            callback(resTweets.statusCode);
+            return;
+        }
+        let bodyTweets = "";
+        resTweets.on("data", (chunk) => {
+            bodyTweets += chunk;
+        });
+        resTweets.on("end", () => {
+            let tweets = JSON.parse(bodyTweets);
+            // console.log("tweets ...", tweets);
+            callback(null, tweets);
         });
     };
     const reqTweets = https.request(optTweets, cbTweets);
@@ -88,12 +87,13 @@ module.exports.filterTweets = (tweets) => {
             href = tweets[i].entities.urls[0].url;
             // console.log(url);
         }
-        let tickerText = tweets[i].full_text;
+        let tickerText =
+            "*" + tweets[i].user.screen_name + "* " + tweets[i].full_text;
         // console.log(tickerText);
         let text = tickerText.substring(0, 50) + "...";
         // console.log(runningText, url);
         tweetArray.push({ href, text });
-        // console.log(tweetArray);
+        console.log(tweetArray);
     }
     return tweetArray;
 };
