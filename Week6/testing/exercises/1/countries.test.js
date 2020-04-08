3d0b388e20e4e10141896c42c8272e8af221e933
ANGELIKA KIM <alika852@gmail.com>
2020-04-08 16:57:13 +0200

    first version of first testing exercise

---
+++ Week6/testing/exercises/1/countries.test.js
@@ -0,0 +1,21 @@
+const { find } = require("./countries");
+
+test("When find is passed an empty string, it returns an empty array", () => {
+    const result = find("");
+    expect(result).toStrictEqual([]);
+});
+
+test("The search is case insensitive", () => {
+    const result = find("germany");
+    expect(result).toStrictEqual(["Germany"]);
+});
+
+test("If there are no matching countries, an empty array is returned", () => {
+    expect(find("blubb")).toStrictEqual([]);
+});
+
+test("The array that it returns contains no more than four matches", () => {
+    const result = find("A");
+    expect(result.length).toBeLessThanOrEqual(4);
+    console.log(find("a"));
+});
